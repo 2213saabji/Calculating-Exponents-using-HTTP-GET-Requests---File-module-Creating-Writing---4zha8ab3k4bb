6a784bcc86c52fd72f947dcc6160e4f45dffee63
Gurpreet Singh <124036886+2213saabji@users.noreply.github.com>
2024-02-25 00:36:40 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -12,12 +12,12 @@ const server = http.createServer((req, res) => {
       const value1 = obj.num1;
       const value2 = obj.num2;
       if(!value1 || !value2){
-        res.writeHead(400,{"Content-Type":"text/plain"})
-        res.end("The operation cannot be performed");
+        res.writeHead(404,{"Content-Type":"text/plain"})
+        res.end("the operation cannot be performed");
       }
       else if(parseInt(value1)<=0 || parseInt(value2)<=0){
-        res.writeHead(400,{"Content-Type":"text/plain"})
-        res.end("The operation cannot be performed");
+        res.writeHead(404,{"Content-Type":"text/plain"})
+        res.end("the operation cannot be performed");
       }
       else{
         res.writeHead(200,{"Content-Type":"text/plain"})
